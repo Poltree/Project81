9cf5e4391d6376336a0486326cbec6aac2e9a81a
Poltree <76885489+Poltree@users.noreply.github.com>
2021-04-10 11:08:34 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,52 +1,41 @@
 
-		var c=document.getElementById("bluecircle");
-		var ctx=c.getContext("2d");
-		ctx.beginPath();
-		ctx.arc(130,130,90,0,2*Math.PI);
-		ctx.lineWidth=10;
-		ctx.strokeStyle="blue";
-		ctx.stroke();
+canvas=document.getElementById("MyCanvas")
+color="red";
+ctx= canvas.getContext("2d")
+ctx.beginPath();
+ctx.strokeStyle = color;
+ctx.lineWidth = 5;
+ctx.arc(550,200,50,0, 2* Math.PI);
+ctx.stroke();
 
-		ctx.beginPath();
-		ctx.arc(330,130,90,0,2*Math.PI);
-		ctx.strokeStyle="black";
-		ctx.stroke();
-		
+ctx1= canvas.getContext("2d")
+color1= "blue";
+ctx1.beginPath();
+ctx1.strokeStyle = color1;
+ctx1.lineWidth = 5;
+ctx1.arc(250,200,50,0, 2* Math.PI);
+ctx1.stroke();
 
-		ctx.beginPath();
-		ctx.arc(530,130,90,0,2*Math.PI);
-		ctx.strokeStyle="red";
-		ctx.stroke();
-		
+ctx2= canvas.getContext("2d")
+color2= "black";
+ctx2.beginPath();
+ctx2.strokeStyle = color2;
+ctx2.lineWidth = 5;
+ctx2.arc(400,200,50,0, 2* Math.PI);
+ctx2.stroke();
 
-		ctx.beginPath();
-		ctx.arc(230,230,90,0,2*Math.PI);
-		ctx.strokeStyle="orange";
-		ctx.stroke();
+ctx3= canvas.getContext("2d")
+color3= "yellow";
+ctx3.beginPath();
+ctx3.strokeStyle = color3;
+ctx3.lineWidth = 5;
+ctx3.arc(325,250,50,0, 2* Math.PI);
+ctx3.stroke();
 
-		ctx.beginPath();
-		ctx.arc(430,230,90,0,2*Math.PI);
-		ctx.strokeStyle="green";
-		ctx.stroke();
-		
-		ctx.beginPath();
-		ctx.arc(130,130,90,0*Math.PI,0.1*Math.PI);
-		ctxx.strokeStyle="blue";
-		ctx.stroke();
-
-		ctx.beginPath();
-		ctx.arc(330,130,90,0.5*Math.PI,0.6*Math.PI);
-		ctx.strokeStyle="black";
-		ctx.stroke();
-		
-
-		ctx.beginPath();
-		ctx.arc(330,130,90,0,0.2*Math.PI);
-		ctx.strokeStyle="black";
-		ctx.stroke();
-		
-
-		ctx.beginPath();
-		ctx.arc(530,130,90,0.5*Math.PI,0.6*Math.PI);
-		ctx.strokeStyle="red";
-		ctx.stroke();
+ctx4= canvas.getContext("2d")
+color4= "green";
+ctx4.beginPath();
+ctx4.strokeStyle = color4;
+ctx4.lineWidth = 5;
+ctx4.arc(475,250,50,0, 2* Math.PI);
+ctx4.stroke();
